a803ea00fbb94b254854f8ebee1c646954f71087
z2wenfa <z2wenfa@gmail.com>
2021-04-17 17:38:31 +0800

    添加Center控件Demo

---
+++ lib/layout/CenterDemo.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/**
+ *  显示一个控件,并使其居中显示,是Align组件的一种精简模式
+ *  【child】:孩子控件 【Widegt】
+ */
+class CenterDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Icon(Icons.ac_unit),
+    );
+  }
+}
